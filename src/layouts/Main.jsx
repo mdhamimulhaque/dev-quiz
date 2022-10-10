d4c1c14f2792403d058800ecef011ab80926ec09
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-10-10 23:13:04 +0600

    header done with route

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 import Footer from '../components/Footer/Footer';
 import Header from '../components/Header/Header';
 
@@ -6,6 +7,7 @@ const Main = () => {
     return (
         <main>
             <Header />
+            <Outlet />
             <Footer />
         </main>
     );
--- src/utilities/router.js
+++ src/utilities/router.js
@@ -1,13 +1,22 @@
 import Main from "../layouts/Main";
 import Topic from "../components/Topic/Topic";
+import Statistics from "../components/Statistics/Statistics";
+import Blog from "../components/Blog/Blog";
+import Contact from "../components/Contact/Contact";
+import Error from "../components/Error/Error";
 import { createBrowserRouter } from "react-router-dom";
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <Main />,
+        errorElement: <Error />,
         children: [
-            { path: "/", element: <Topic /> }
+            { path: "/", element: <Topic /> },
+            { path: "/topic", element: <Topic /> },
+            { path: "/statistics", element: <Statistics /> },
+            { path: "/blog", element: <Blog /> },
+            { path: "/contact", element: <Contact /> },
         ]
     }
 ])
